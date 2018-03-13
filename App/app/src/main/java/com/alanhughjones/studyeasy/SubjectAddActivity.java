64de85a65f2cf696127cac4df99964fe6dd55efa
AlanJonesDIT <c16719689@mydit.ie>
2018-03-13 23:39:28 +0000

    Checking branch working

--- App/app/src/main/java/com/alanhughjones/studyeasy/SubjectAddActivity.java
+++ App/app/src/main/java/com/alanhughjones/studyeasy/SubjectAddActivity.java
@@ -9,5 +9,7 @@ public class SubjectAddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject_add);
+
+        // comment delete
     }
 }
