03755a9e31d10173628cad88ad22fdc302bf2d2e
AlanJonesDIT <c16719689@mydit.ie>
2018-03-14 21:24:26 +0000

    Added PT Brazil locale for text - changed add task date input to button - current date now shows on datepicker

--- App/app/src/main/java/com/alanhughjones/studyeasy/NewTaskActivity.java
+++ App/app/src/main/java/com/alanhughjones/studyeasy/NewTaskActivity.java
@@ -1,13 +1,65 @@
 package com.alanhughjones.studyeasy;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class NewTaskActivity extends AppCompatActivity {
+
+
+    Button datePick;
+    int year_x,month_x,day_x;
+    static final int DIALOG_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
+
+        final Calendar cal = Calendar.getInstance();
+        year_x = cal.get(Calendar.YEAR);
+        month_x = cal.get(Calendar.MONTH);
+        day_x = cal.get(Calendar.DAY_OF_MONTH);
+
+        showDialogOnInputClick();
     }
+
+    public void showDialogOnInputClick() {
+       datePick = findViewById(R.id.datepick_btn);
+
+       datePick.setOnClickListener(
+               new View.OnClickListener() {
+                   @Override
+                   public void onClick(View v) {
+                        showDialog(DIALOG_ID);
+                   }
+               }
+       );
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if(id == DIALOG_ID)
+            return new DatePickerDialog(this, dpickerListener, year_x, month_x, day_x);
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener dpickerListener
+            = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            year_x = year;
+            month_x = monthOfYear+1;
+            day_x = dayOfMonth;
+            Toast.makeText(NewTaskActivity.this, day_x + "/" + month_x + "/" + year_x, Toast.LENGTH_SHORT).show();
+        }
+    };
 }
