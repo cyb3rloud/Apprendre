8bbf5a99f9640e35c92740b3d012e266ebb7f0e3
AlanJonesDIT <c16719689@mydit.ie>
2018-03-25 10:16:41 +0100

    Subjects can now be added to database - upon successful entry subjects are shown in list

--- App/app/src/main/java/com/alanhughjones/studyeasy/DatabaseHelper.java
+++ App/app/src/main/java/com/alanhughjones/studyeasy/DatabaseHelper.java
@@ -51,7 +51,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public Cursor getAllData() {
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor result = db.rawQuery("select * from " + TABLE_NAME,null);
+        String query = "SELECT * FROM " + TABLE_NAME;
+        Cursor result = db.rawQuery(query,null);
         return result;
     }
 }
--- App/app/src/main/java/com/alanhughjones/studyeasy/SubjectListActivity.java
+++ App/app/src/main/java/com/alanhughjones/studyeasy/SubjectListActivity.java
@@ -1,18 +1,43 @@
 package com.alanhughjones.studyeasy;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class SubjectListActivity extends AppCompatActivity {
 
+    private static final String TAG = "SubjectListActivity";
     DatabaseHelper myDB;
+    private ListView subjectListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject_list);
+        subjectListView = findViewById(R.id.subj_list);
+        myDB = new DatabaseHelper(this);
+
+        populateSubjectList();
+    }
+
+    private void populateSubjectList() {
+        Log.d(TAG, "populateSubjectList: Displaying subjects in the ListView.");
+        Cursor subjects = myDB.getAllData();
+        ArrayList<String> listSubjects = new ArrayList<>();
+        while (subjects.moveToNext()){
+            listSubjects.add(subjects.getString(1));
+        }
+        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,listSubjects);
+        subjectListView.setAdapter(adapter);
     }
 
     /** Called when user clicks + button **/
@@ -20,5 +45,9 @@ public class SubjectListActivity extends AppCompatActivity {
         // Open subject 'add activity' when button is clicked
         Intent intent = new Intent(this, SubjectAddActivity.class); // change back to SubjectAddActivity
         startActivity(intent);
+    }
+
+    private void toastMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
